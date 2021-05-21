30cf5058c4acc38a780a53a954058c16eec9f8d7
kali <kali@kali.kali>
2021-05-20 21:31:23 -0300

    criacao do objeto inicial e controle de sessao

--- Drive.py
+++ Drive.py
@@ -1,14 +1,17 @@
 import pygame
 pygame.init()
 
-janela = pygame.display.set_mode((800,600))
+janela = pygame.display.set_mode((1024,800))
 pygame.display.set_caption("Criando um jogo com Python")
 
 janela_aberta = True
 while janela_aberta :
-
+    pygame.time.delay(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             janela_aberta = False
+
+            pygame.draw.circle(janela, (255,0,0), (400,300),50)
+            pygame.display.update()
 
 pygame.quit()
