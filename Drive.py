45d978cd6679609dcb460c1e2b06d334a0e2cf51
kali <kali@kali.kali>
2021-05-20 21:27:22 -0300

    criacao condicional quit janela

--- Drive.py
+++ Drive.py
@@ -3,3 +3,12 @@ pygame.init()
 
 janela = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Criando um jogo com Python")
+
+janela_aberta = True
+while janela_aberta :
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            janela_aberta = False
+
+pygame.quit()
